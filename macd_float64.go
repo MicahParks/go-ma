641f1307b29bb4d939523ac21e8980540bf15244
Micah Parks <git@micahparks.com>
2021-10-12 09:44:07 -0400

    Confirmed SMA and EMA

--- macd_float64.go
+++ macd_float64.go
@@ -2,8 +2,8 @@ package ma
 
 // TODO
 type MACD struct {
-	Long  *EMAFloat
-	Short *EMAFloat
+	Long  *EMAFloat // Typical 26 periods.
+	Short *EMAFloat // Typical 12 periods.
 }
 
 // TODO
@@ -18,3 +18,5 @@ func NewMACDFloat(long, short *EMAFloat) (macd MACD) {
 func (macd MACD) Calculate(next float64) (result float64) {
 	return macd.Short.Calculate(next) - macd.Long.Calculate(next)
 }
+
+// TODO Make the "signal line" from the result of the MACD. Typically nine periods.
